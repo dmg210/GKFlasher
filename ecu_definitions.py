e5dbe3ead4234324882faa87d55c303820c58397
Piotr Zdolski <dante383@protonmail.com>
2025-12-06 19:11:26 +0100

    initial sim2k (elantra) support

--- ecu_definitions.py
+++ ecu_definitions.py
@@ -80,7 +80,20 @@ ECU_IDENTIFICATION_TABLE = [
 			'program_section_address': 0x90000,
 			'program_section_size': 0x70000
 		}
-	},	
+	},
+	{
+		'offset': 0x92000,
+		'expected': [b'\x36\x36\x35\x30'], # ca66
+ 		'ecu': {
+			'name': 'SIMK2K (Elantra 2.0 Beta)',
+			'eeprom_size_bytes': 524288,
+			'bin_offset': -0x80000,
+			'calibration_section_address': 0x92000,
+			'calibration_size_bytes': 0x12000,
+			'program_section_address': 0x24000,
+			'program_section_size': 0x5BFFF
+		}
+	},
 ]
 
 BAUDRATES = {
